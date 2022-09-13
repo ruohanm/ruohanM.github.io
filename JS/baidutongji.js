970109c14a4ae7dcf564944399c6440d6b30df5c
ruohanm <113405053+ruohanm@users.noreply.github.com>
2022-09-14 04:59:23 +0800

    Delete JS directory

--- JS/baidutongji.js
+++
@@ -1,9 +0,0 @@
-<script>
-var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?d5f327b1464b90339e8cdc5304de05ec";
-  var s = document.getElementsByTagName("script")[0]; 
-  s.parentNode.insertBefore(hm, s);
-})();
-</script>
