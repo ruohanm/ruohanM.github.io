6d48a67724c9c0a714bb586805b198ea4f88a1dd
ruohanm <113405053+ruohanm@users.noreply.github.com>
2024-07-17 19:04:16 +0800

    20240717转移

---
+++ js/bd.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?511b4c86f86478086cb8b04c8122cfa3";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
